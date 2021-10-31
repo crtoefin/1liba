1ac3f04b953798f87131c448e7529c7ee2872412
crtoefin <93145187+crtoefin@users.noreply.github.com>
2021-10-31 13:58:38 +0300

    Add files via upload

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,27 +12,50 @@
 
 #include "libft.h"
 
+static int	ft_len_sign(int n, int *np)
+{
+	int	len;
+
+	if (n <= 0)
+		*np = 1;
+	else
+		*np = 0;
+	len = 1;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len + *np);
+}
+
+void	ft_fill_string(char *str, int last,
+	int num, int neg)
+{
+	while (num)
+	{
+		if (neg)
+			str[last--] = 48 + ((num % 10) * (-1));
+		else
+			str[last--] = 48 + (num % 10);
+		num /= 10;
+	}
+}
+
 char	*ft_itoa(int n)
 {
+	int		len;
+	int		np;
 	char	*str;
 
-	str = (char *)malloc(sizeof(char) * 2);
-	if (str == NULL)
-		return (NULL);
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
+	len = ft_len_sign(n, &np);
+	str = ft_calloc((size_t)(len), sizeof(char));
+	if (!(str))
+		return ((void *)0);
+	if (n == 0)
+		str[0] = '0';
 	if (n < 0)
-	{
 		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
-	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
+	ft_fill_string(str, (len - 2), n, np);
 	return (str);
 }
